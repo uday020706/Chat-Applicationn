78a2072835c5d872d2a3f5bbb3a008228ac11f88
Uday Chandra <udaychandra266@gmail.com>
2026-02-04 19:32:39 +0530

    Updated url

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -4,6 +4,8 @@ import { auth } from "../firebase/firebaseConfig";
 import AuthCard from "../components/AuthCard";
 
 export default function Register() {
+  const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [userName, setUserName] = useState("")
@@ -22,7 +24,7 @@ export default function Register() {
       alert("✅ Registered Successfully!");
 
       // ✅ Step 2: Save user in MongoDB
-      await fetch("http://localhost:5000/api/users", {
+      await fetch(`${BASE_URL}/api/users`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
